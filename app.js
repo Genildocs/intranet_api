b7f7a4c59db6c0ebe5a2b042c2875f8c9355eace
genildocs <genildocs@gmail.com>
2023-12-29 17:08:42 -0300

    commit

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
-const express = require("express");
-const morgan = require("morgan");
+const express = require('express');
+const morgan = require('morgan');
 const app = express();
 
-
 // MIDDLEWARES
-if(process.env.NODE_ENV === "development") {
-    app.use(morgan("dev"));
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
 }
 app.use(express.json());
 
