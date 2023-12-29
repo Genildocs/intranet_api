a4762e18c46b7edc3d77d070f7e9d1a10968b06c
genildocs <genildocs@gmail.com>
2023-12-29 07:38:06 -0300

    implementing mongo db

--- server.js
+++ server.js
@@ -1,9 +1,20 @@
+const mongoose = require("mongoose");
 const dotenv = require("dotenv");
-dotenv.config({ path: "./config.env" });
+dotenv.config( {path: './.env'} );
+
+
+mongoose.set('strictQuery', false)
+
+const DB = process.env.MONGODB_URL.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
+mongoose.connect(DB).then(con => {
+  console.log("Conexão com o banco de dados realizada com sucesso!");
+}).catch(err => {
+  console.log("Erro ao conectar com o banco de dados: " + err);
+});
+
 
 const app = require("./app");
-
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Servidor iniciado na porta: ${PORT}`);
 });
--- utils/config.js
+++
@@ -1,4 +0,0 @@
-const PORT = process.env.PORT;
-const FIREBASE_URL = process.env.FIREBASE_URL;
-
-module.exports = { PORT, FIREBASE_URL };
